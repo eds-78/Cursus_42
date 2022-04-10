feea66acdfed7a1967ebd734c67f45b4ffd22e6d
Erick DE SMET <erickdesmet@MacBook-Pro-de-Erick.local>
2022-04-10 17:25:00 +0200

    ajout de fonctions

--- libft/libft.h
+++ libft/libft.h
@@ -6,13 +6,22 @@
 /*   By: ede-smet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/05 00:33:56 by ede-smet          #+#    #+#             */
-/*   Updated: 2022/04/10 03:45:19 by ede-smet         ###   ########.fr       */
+/*   Updated: 2022/04/10 15:35:21 by ede-smet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
+/* for size_t import */
+# include <stddef.h>
 
-int	ft_atoi(char *str);
+int		ft_atoi(char *str);
+int		ft_isapha(char c);
+int		ft_isdigit(char c);
+int		ft_isalnum(char c);
+int		ft_isascii(char c);
+int		ft_isprint(char c);
+int		ft_strlen(char *str);
+void	*ft_memset(void *s, int c, size_t n);
 
 #endif
